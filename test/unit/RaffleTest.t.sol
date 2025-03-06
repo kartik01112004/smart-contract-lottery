2f3a0649587e8bb9ec5110e7e287ba465f8124b1
kartik01112004 <kartikgoel018@gmail.com>
2025-03-06 15:41:11 +0530

    testing upkeepNeeded

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -109,4 +109,37 @@ contract RaffleTest is Test {
     // Challenge
     //testCheckUpkeepReturnsFalseIfEnoughTimeHasPassed
     //testCheckUpkeepReturnsTrueWhenParametersAreGood
+
+    function testPerformUpkeepCanOnlyRunIfChekcUpkeepIsTrue() public {
+        //Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        // Act/Assert
+        raffle.performUpkeep("");
+    }
+
+    function testPerformUpkeepRevetrsIfCheckUpkeepIsFalse() public {
+        //Arrange
+        uint256 currentBalance = 0;
+        uint256 numPlayers = 0;
+        Raffle.RaffleState rState = raffle.getRaffleState();
+
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        currentBalance = currentBalance + entranceFee;
+        numPlayers = 1;
+
+        //Act / Assert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Raffle.Raffle__UpkeepNotNeeded.selector,
+                currentBalance,
+                numPlayers,
+                rState
+            )
+        );
+        raffle.performUpkeep("");
+    }
 }
